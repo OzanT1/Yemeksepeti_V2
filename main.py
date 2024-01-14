b3e65740caf69fdec57b0d89635d17314d7813f9
Ozan <103127960+OzanT1@users.noreply.github.com>
2024-01-14 15:56:05 +0300

    End Purchase Done!!!

--- main.py
+++ main.py
@@ -691,9 +691,25 @@ def customer_page(login_customer, email, password):
             price_index = 2
             total_price += float(item[price_index])
 
-        print("Total Price: ", total_price)
         total_price_label.config(text=f"Total Price: ${total_price}")
 
+    def end_purchase():
+        selected_payment_option = payment_combobox.get()
+        selected_items = basket_listbox.get(0, tk.END)
+
+        # Inserting order to the database
+        sql_cmd = "insert into Orders(orderDate, paymentMethod, customerID) values(CURRENT_DATE(), %s, %s)"
+        mycursor.execute(sql_cmd, (selected_payment_option, customer_id))
+        mydb.commit()
+
+        # Filling OrderDetails
+        
+
+
+        print(f"Selected Payment Option: {selected_payment_option}")
+        print("Items in the Basket:")
+        for item in selected_items:
+            print(item)
 
     if authenticate_customer(email, password):
 
@@ -764,8 +780,15 @@ def customer_page(login_customer, email, password):
         total_price_label = tk.Label(basket_list_frame, text="Total Price: $0")
         total_price_label.pack()
 
-        end_purchase_button = tk.Button(basket_list_frame, text="End Purchase")
+        end_purchase_button = tk.Button(basket_list_frame, text="End Purchase", command=end_purchase)
         end_purchase_button.pack(side=tk.TOP)
+
+        # Create Combobox for payment options
+        payment_options = ['Cash', 'Credit Card']
+        payment_combobox = ttk.Combobox(basket_list_frame, values=payment_options)
+        payment_combobox.set(payment_options[0])  # Set the default value
+        payment_combobox.pack(pady=10)
+
 
 
 
